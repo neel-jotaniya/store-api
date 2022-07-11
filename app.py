172f478a1171cce5d45b88246d6e5063b12bade7
neel-jotaniya <jotaniyaneel07@gmail.com>
2022-07-11 12:43:25 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -4,3 +4,9 @@ from db import db
 @app.before_first_request
 def create_table():
     db.create_all()
+    
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+    
+    app.run(port=5000, debug= True)
